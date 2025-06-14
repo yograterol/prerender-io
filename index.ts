1088b17801d03910cc29fbda5dab2d9c8f47de41
Yohan Graterol <yohangraterol92@gmail.com>
2025-06-14 17:55:05 -0500

    remove material design icons

--- index.ts
+++ index.ts
@@ -298,7 +298,7 @@ async function render(browser, full, dev, ua) {
   await page.setUserAgent(ua || DEFAULT_UA[dev]);
   await page.setViewport(VIEWPORT[dev]);
 
-  const FONT_OK = [host, `fonts.gstatic.com`, `cdnjs.cloudflare.com`, `cdn.materialdesignicons.com`];
+  const FONT_OK = [host, `fonts.gstatic.com`, `cdnjs.cloudflare.com`,]; // Removed  `cdn.materialdesignicons.com`
 
   /*──── Request interception ────*/
   await page.setRequestInterception(true);
